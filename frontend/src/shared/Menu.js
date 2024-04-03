9ec1ad89a051a7a96b226b82109018aa12a2b133
Sagar Bisht <itssagar11@gmail.com>
2024-04-03 12:07:53 +0530

    frontend basic done

---
+++ frontend/src/shared/Menu.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Outlet } from "react-router-dom";
+import './style.css'
+export default function Menu() {
+  return (
+    <div className="container">
+        <div className='menu'>
+        <a href='/'>Customer</a>
+            <a href='/deposit'>New Deposit</a>
+            <a href='/withdrawal'>withdrawal</a>
+            <a href='/cheque'>Cheque Deposit</a>
+            <a href='/balance'>Balance</a>
+         
+        </div>
+        
+        <div className=" main">
+          <Outlet />
+        </div>
+      
+      
+      
+      </div>
+  
+  )
+}
